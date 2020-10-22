8417d7b5c36272e593c698966fc883f1986b9402
J Akkermans <99044668@mydavinci.nl>
2020-10-22 08:42:32 +0200

    Done
    Simple love calc with math.floor and prompts.

---
+++ script.js
@@ -0,0 +1,7 @@
+var userNameOne = prompt("What is your name?");
+var userNameTwo = prompt("What is the name of the person you admire?");
+
+var num = Math.random() * 100;
+num = Math.floor(num) + 1;
+alert("The love between " + userNameOne + " and " + userNameTwo + " is " + num);
+
